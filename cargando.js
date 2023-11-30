24018c79bdf64a3fd38e9133a077a7162b96873d
gustavo1viveros <126522555+gustavo1viveros@users.noreply.github.com>
2023-11-30 19:44:08 -0300

    Add files via upload

---
+++ cargando.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Oculta el mensaje de carga después de que la página se ha cargado completamente
+    document.getElementById("loading-message").style.display = "none";
+});
